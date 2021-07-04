c7c96865e4e6bd5e91abfbe04ff0b792b1ad89ab
csloz <lawrence@computersolutions.cn>
2021-07-04 16:13:41 +0700

    adding bouncy scroll to park map page (goes to the lower portion using bounce.js)

--- assets/js/bounce.js
+++ assets/js/bounce.js
@@ -1,13 +1,19 @@
+$(document).ready(function() {
  $(window).on('scroll', function() {
 
-    if ($(window).scrollTop() + $(window).height() > $('.wrapper').outerHeight()) {
-      $('.arrow').hide();
+    if ($(window).scrollTop() + $(window).height() > 1200) {
+      $('.bounce_arrow').hide();
     } else {
-      $('.arrow').show();
+      $('.bounce_arrow').show();
     }
   });
 
 
-$('.arrow').click(function(){
-   $("html").animate({ scrollTop: $('html').prop("scrollHeight")}, 1200);
+  $('.bounce_arrow').click( function(){
+      // document.querySelector('.download_map_button').scrollIntoView({ behavior: 'smooth' });
+$('html, body').animate({
+          scrollTop: $('.safety_measures').offset().top+50
+        }, 1000);
+  });
+
 });
